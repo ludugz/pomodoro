ce270d1c8856784bbb9f008d43cf75718d16008d
Tan N. Truong <tntan92@gmail.com>
2023-05-27 22:27:44 +0900

    Replace default code in MainActivity

--- app/src/main/java/ludugz/pomodoro/MainActivity.kt
+++ app/src/main/java/ludugz/pomodoro/MainActivity.kt
@@ -10,6 +10,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import ludugz.pomodoro.ui.components.Clock
+import ludugz.pomodoro.ui.theme.Lima300
 import ludugz.pomodoro.ui.theme.PomodoroTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,30 +19,13 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             PomodoroTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = Lima300
                 ) {
-                    Greeting("Android")
+                    Clock()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PomodoroTheme {
-        Greeting("Android")
     }
 }

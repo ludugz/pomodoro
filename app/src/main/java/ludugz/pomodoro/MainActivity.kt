5856fc8ce8fac28f35e4316f22912d1d93def224
Tan N. Truong <tntan92@gmail.com>
2023-06-03 23:13:41 +0900

    Add preview for main page component

--- app/src/main/java/ludugz/pomodoro/MainActivity.kt
+++ app/src/main/java/ludugz/pomodoro/MainActivity.kt
@@ -7,8 +7,10 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import ludugz.pomodoro.ui.components.Clock
 import ludugz.pomodoro.ui.components.Wave
 import ludugz.pomodoro.ui.theme.Lima300
@@ -45,3 +47,8 @@ fun PomodoroPage() {
 
 }
 
+@Preview
+@Composable
+fun PreviewPomodoroPage() {
+    PomodoroPage()
+}

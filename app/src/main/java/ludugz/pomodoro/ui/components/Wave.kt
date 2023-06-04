883070d439fc4a0f98353041168a720c2a3b003a
Tan N. Truong <tntan92@gmail.com>
2023-06-04 14:24:42 +0900

    Replace Canvas with Box for Wave composable.

--- app/src/main/java/ludugz/pomodoro/ui/components/Wave.kt
+++ app/src/main/java/ludugz/pomodoro/ui/components/Wave.kt
@@ -6,6 +6,8 @@ import androidx.compose.animation.core.animate
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
@@ -15,11 +17,16 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.launch
+import ludugz.pomodoro.ui.theme.Lima300
 import ludugz.pomodoro.ui.theme.Pink40
 
 /**
@@ -29,7 +36,7 @@ import ludugz.pomodoro.ui.theme.Pink40
 
 @Composable
 fun Wave(modifier: Modifier = Modifier, targetHeight: Dp = 400.dp) {
-    var strokeWidthState by remember { mutableStateOf(0.dp) }
+    var boxHeight by remember { mutableStateOf(0.dp) }
     LaunchedEffect(key1 = Unit) {
         launch {
             animate(
@@ -37,18 +44,21 @@ fun Wave(modifier: Modifier = Modifier, targetHeight: Dp = 400.dp) {
                 targetValue = targetHeight.value,
                 animationSpec = tween(durationMillis = 3000)
             ) { value, _ ->
-                strokeWidthState = value.dp
+                boxHeight = value.dp
             }
         }
     }
-    Canvas(modifier = modifier.height(height = strokeWidthState)) {
-        drawLine(
-            strokeWidth = drawContext.size.height,
-            start = Offset(0f, 0f),
-            end = Offset(size.width, 0f),
-            color = Pink40
-        )
-    }
+
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .height(height = boxHeight)
+            .background(
+                brush = Brush.verticalGradient(
+                    colors = listOf(Pink40, Lima300)
+                )
+            )
+    )
 }
 
 @Preview(

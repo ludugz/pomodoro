088f355f1d57b20018a097185a47ed87dc1dc9cd
Tan N. Truong <tntan92@gmail.com>
2023-06-04 14:24:14 +0900

    Init Timer used for Clock

--- app/src/main/java/ludugz/pomodoro/ui/components/Clock.kt
+++ app/src/main/java/ludugz/pomodoro/ui/components/Clock.kt
@@ -11,7 +11,8 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -21,7 +22,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.StrokeCap
@@ -134,7 +134,16 @@ fun BorderOuterCircle(
     }
 }
 
-@Preview(backgroundColor = 0xFFcdeda5, showBackground = true)
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Timer(modifier: Modifier = Modifier) {
+    TextField(
+        value = "",
+        onValueChange = { text ->
+
+        }
+    )
+}
 
 @Preview(name = "Clock", backgroundColor = 0xFFcdeda5, showBackground = true)
 @Composable

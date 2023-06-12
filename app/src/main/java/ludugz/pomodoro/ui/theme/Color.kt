a756a17d55d858ede7cc16f3ff40c922bc9da3e7
Tan N. Truong <tntan92@gmail.com>
2023-06-12 20:35:06 +0900

    Update colors

--- app/src/main/java/ludugz/pomodoro/ui/theme/Color.kt
+++ app/src/main/java/ludugz/pomodoro/ui/theme/Color.kt
@@ -10,4 +10,11 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
+// Lima (Green)
 val Lima300 = Color(0xFFcdeda5)
+val Lima200 = Color(0xFFe6f5c1)
+val Lima100 = Color(0xFFf4fae3)
+
+// Rock (Brown)
+val Rock300 = Color(0xFFb8aba5)
+val Rock500 = Color(0xFF4e3b3b)

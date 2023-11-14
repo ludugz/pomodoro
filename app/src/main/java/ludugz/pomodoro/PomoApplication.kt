37cf789dc9e91a62ec974321368fc2fab0e45a20
Tan N. Truong <tntan92@gmail.com>
2023-11-14 21:46:01 +0900

    Add Pomo application

---
+++ app/src/main/java/ludugz/pomodoro/PomoApplication.kt
@@ -0,0 +1,15 @@
+package ludugz.pomodoro
+
+import android.app.Application
+import timber.log.Timber
+
+/**
+ * Created by Tan N. Truong, on 14 July, 2023
+ * Email: ludugz@gmail.com
+ */
+class PomoApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(Timber.DebugTree())
+    }
+}

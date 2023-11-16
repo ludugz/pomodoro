91b75a5dff51634aca0acbabaa3ac8f7d688d83a
Tan N. Truong <tntan92@gmail.com>
2023-11-16 19:56:04 +0900

    Init Splash screen

---
+++ app/src/main/java/ludugz/pomodoro/ui/pages/SplashPage.kt
@@ -0,0 +1,16 @@
+package ludugz.pomodoro.ui.pages
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+
+/**
+ * Created by Tan N. Truong, on 16 November, 2023
+ * Email: ludugz@gmail.com
+ */
+@Composable
+fun SplashPage(
+    modifier: Modifier = Modifier,
+) {
+
+}

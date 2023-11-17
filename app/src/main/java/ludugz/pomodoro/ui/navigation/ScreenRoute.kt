ad5b84671120432c0cede783868942ac01440983
Tan N. Truong <tntan92@gmail.com>
2023-11-18 00:15:15 +0900

    Add Screen Route for navigation

---
+++ app/src/main/java/ludugz/pomodoro/ui/navigation/ScreenRoute.kt
@@ -0,0 +1,11 @@
+package ludugz.pomodoro.ui.navigation
+
+
+/**
+ * Created by Tan N. Truong, on 16 November, 2023
+ * Email: ludugz@gmail.com
+ */
+enum class ScreenRoute {
+    SPLASH,
+    TIMER
+}

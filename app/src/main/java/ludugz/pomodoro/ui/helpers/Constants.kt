f55625c82e87b95f284c8944db6d00d078dacd2f
Tan N. Truong <tntan92@gmail.com>
2023-11-18 18:09:32 +0900

    Add Constants.kt

---
+++ app/src/main/java/ludugz/pomodoro/ui/helpers/Constants.kt
@@ -0,0 +1,22 @@
+package ludugz.pomodoro.ui.helpers
+
+import androidx.compose.ui.unit.dp
+
+
+/**
+ * Created by Tan N. Truong, on 16 November, 2023
+ * Email: ludugz@gmail.com
+ */
+object Constants {
+    // Splash Page
+
+    // Timer Page
+    const val CIRCLE_RADIUS = 200
+    const val CIRCLE_THICKNESS = 16
+    const val POMODORO_TIMER_DURATION = 25 * 60 * 1000L
+    const val TIMER_COUNT_DOWN_INTERVAL = 1000L
+    const val TAP_SCREEN_TEXT= "TAP SCREEN WHEN YOU GET DISTRACTED"
+
+    // Sample (will be replaced later)
+    const val MOTIVATION_QUOTE = "Fall in love with boredom."
+}

03af47215f3f861d171dfa4efbb795363a56d16f
Tan N. Truong <tntan92@gmail.com>
2024-01-01 22:10:33 +0900

    Update composable on Setting Page

--- app/src/main/java/ludugz/pomodoro/ui/pages/SettingPage.kt
+++ app/src/main/java/ludugz/pomodoro/ui/pages/SettingPage.kt
@@ -17,9 +17,91 @@ enum class SettingSelectableType {
     ColorSelection,
     Expandable,
 }
+
 @Composable
 fun SettingPage(navController: NavController) {
-    Text(text = "Setting Page")
+    Column(
+        modifier = Modifier.padding(8.dp),
+        verticalArrangement = Arrangement.SpaceBetween,
+        horizontalAlignment = Alignment.Start,
+    ) {
+        SettingsLabelTextStyle(text = "Display")
+        SelectableItem(
+            iconResource = R.drawable.dark_mode_icon,
+            label = "Dark Mode",
+            selectionType = SettingSelectableType.Toggle,
+        )
+        SelectableItem(
+            iconResource = R.drawable.color_icon,
+            label = "Background Color",
+            selectionType = SettingSelectableType.ColorSelection,
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        SettingsLabelTextStyle(text = "Feedback")
+        SelectableItem(
+            iconResource = R.drawable.request_feature_icon,
+            label = "Request a feature",
+            selectionType = SettingSelectableType.Expandable,
+        )
+        SelectableItem(
+            iconResource = R.drawable.report_issue_icon,
+            label = "Report an issue",
+            selectionType = SettingSelectableType.Expandable,
+        )
+    }
+}
+
+@Composable
+fun SelectableItem(iconResource: Int, label: String, selectionType: SettingSelectableType) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(64.dp)
+            .padding(
+                horizontal = 16.dp,
+                vertical = 8.dp,
+            ),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween,
+    ) {
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Icon(
+                painter = painterResource(id = iconResource),
+                contentDescription = ""
+            )
+            Text(
+                text = label,
+                modifier = Modifier.padding(16.dp),
+                style = RockTypography.bodyMedium,
+            )
+        }
+
+        when (selectionType) {
+            SettingSelectableType.Toggle -> {
+                Text(
+                    text = "OFF",
+                    style = RockTypography.bodyMedium,
+                )
+            }
+
+            SettingSelectableType.ColorSelection -> {
+                Box(
+                    modifier = Modifier
+                        .padding(vertical = 8.dp)
+                        .fillMaxHeight()
+                        .aspectRatio(1f)
+                        .background(color = Color(BackgroundColors[0]))
+                )
+            }
+
+            SettingSelectableType.Expandable -> {
+                Icon(
+                    painter = painterResource(id = R.drawable.right_arrow_icon),
+                    contentDescription = ""
+                )
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)

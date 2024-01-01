7f428213878f689c5f06367dcb0dbebda1cfb00e
Tan N. Truong <tntan92@gmail.com>
2024-01-01 22:13:07 +0900

    Refactor SettingPage package

--- app/src/main/java/ludugz/pomodoro/ui/pages/setting/SettingPage.kt
+++ app/src/main/java/ludugz/pomodoro/ui/pages/setting/SettingPage.kt
@@ -1,10 +1,31 @@
-package ludugz.pomodoro.ui.pages
+package ludugz.pomodoro.ui.pages.setting
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import ludugz.pomodoro.R
+import ludugz.pomodoro.ui.helpers.SettingsLabelTextStyle
+import ludugz.pomodoro.ui.theme.BackgroundColors
+import ludugz.pomodoro.ui.theme.RockTypography
 
 
 /**

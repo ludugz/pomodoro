842738e51fb0eb44c36c46784da6c7cf05a34c68
Tan N. Truong <tntan92@gmail.com>
2024-01-09 22:07:31 +0900

    Add hilt annotation to Application

--- app/src/main/java/ludugz/pomodoro/PomoApplication.kt
+++ app/src/main/java/ludugz/pomodoro/PomoApplication.kt
@@ -1,12 +1,14 @@
 package ludugz.pomodoro
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
 /**
  * Created by Tan N. Truong, on 14 July, 2023
  * Email: ludugz@gmail.com
  */
+@HiltAndroidApp
 class PomoApplication : Application() {
     override fun onCreate() {
         super.onCreate()

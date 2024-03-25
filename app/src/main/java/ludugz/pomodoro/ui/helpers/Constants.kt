1f13c5a64f9d77a66ed6764b7f85377747806ce8
Tan N. Truong <tntan92@gmail.com>
2024-03-25 21:59:26 +0900

    Update constants

--- app/src/main/java/ludugz/pomodoro/ui/helpers/Constants.kt
+++ app/src/main/java/ludugz/pomodoro/ui/helpers/Constants.kt
@@ -11,7 +11,7 @@ object Constants {
     // Timer Page
     const val CIRCLE_RADIUS = 200
     const val CIRCLE_THICKNESS = 16
-//    const val POMODORO_TIMER_DURATION: Long = 25 * 60
+
     const val POMODORO_TIMER_DURATION: Long = 10
     const val TIMER_COUNT_DOWN_INTERVAL = 1000L
     const val TAP_SCREEN_TEXT= "Tap screen when you get distracted"
@@ -19,6 +19,13 @@ object Constants {
 
     // Cheering dialog
     const val CHEERING_DIALOG_CONFIRM_BUTTON = "Let me try"
+
+
+    // Key
+    const val KEY_SCREEN_ROUTE = "KEY_SCREEN_ROUTE"
+
+    // Bundle Key
+    const val BUNDLE_KEY_SETTING_BACKGROUND_BO = "BUNDLE_KEY_SETTING_BACKGROUND_BO"
 
     // Sample (will be replaced later)
     const val MOTIVATION_QUOTE = "Fall in love with boredom."

fa882f766168bcc034856e7bd97b0346bd44fdea
Tan N. Truong <tntan92@gmail.com>
2024-05-15 21:50:34 +0900

    Create RoundedButton.kt composable

---
+++ app/src/main/java/ludugz/pomodoro/ui/components/RoundedButton.kt
@@ -0,0 +1,45 @@
+package ludugz.pomodoro.ui.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import ludugz.pomodoro.R
+import ludugz.pomodoro.ui.helpers.Constants.EMPTY_STRING
+
+
+/**
+ * Created by Tan N. Truong, on 15 May, 2024
+ * Email: ludugz@gmail.com
+ */
+
+@Composable
+fun RoundedButton(
+    modifier: Modifier = Modifier,
+    resource: Int,
+    contentDescription: String = EMPTY_STRING,
+    backGroundColor: Color = Color.White,
+    onClick: () -> Unit = { },
+) {
+    Button(
+        modifier = modifier.background(color = backGroundColor),
+        shape = RoundedCornerShape(50),
+        onClick = onClick,
+    ) {
+        Icon(
+            painter = painterResource(id = resource),
+            contentDescription = contentDescription,
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewRoundedButton() {
+    RoundedButton(resource = R.drawable.ic_play_transparent_24, contentDescription = "Play")
+}

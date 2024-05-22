104c45d0287c45e9994a4702e13204718a9eec88
Tan N. Truong <tntan92@gmail.com>
2024-05-22 23:48:40 +0900

    Init FocusZone.kt component

---
+++ app/src/main/java/ludugz/pomodoro/ui/components/FocusZone.kt
@@ -0,0 +1,23 @@
+package ludugz.pomodoro.ui.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import java.lang.reflect.Modifier
+
+
+/**
+ * Created by Tan N. Truong, on 22 May, 2024
+ * Email: ludugz@gmail.com
+ */
+@Composable
+fun FocusZone(
+    modifier: Modifier = Modifier(),
+) {
+    // TODO: Implement FocusZone composable
+}
+
+@Composable
+@Preview
+fun PreviewFocusZone() {
+    FocusZone()
+}

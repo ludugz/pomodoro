482192fe24f86f1a440b77f36c3a5cdbf8735084
Tan N. Truong <tntan92@gmail.com>
2024-05-27 22:44:04 +0900

    Update UI for FocusZone.kt

--- app/src/main/java/ludugz/pomodoro/ui/components/FocusZone.kt
+++ app/src/main/java/ludugz/pomodoro/ui/components/FocusZone.kt
@@ -1,11 +1,14 @@
 package ludugz.pomodoro.ui.components
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import ludugz.pomodoro.ui.helpers.formattedTime
@@ -22,10 +25,14 @@ fun FocusZone(
 ) {
     Box(modifier = Modifier
         .fillMaxSize()
+        .background(color = Color.Black)
         .clickable { onClick() }) {
         Text(
+            modifier = Modifier
+                .align(alignment = Alignment.Center),
             text = timeLeft.formattedTime(),
             fontSize = 32.sp,
+            color = Color.White,
         )
     }
 }

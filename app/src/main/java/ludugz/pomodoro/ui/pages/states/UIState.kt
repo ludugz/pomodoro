5ffc4d791f41ee62d056843a0837c4f603953db2
Tan N. Truong <tntan92@gmail.com>
2024-06-02 23:42:51 +0900

    Create UIState.kt

---
+++ app/src/main/java/ludugz/pomodoro/ui/pages/states/UIState.kt
@@ -0,0 +1,14 @@
+package ludugz.pomodoro.ui.pages.states
+
+
+/**
+ * Created by Tan N. Truong, on 31 May, 2024
+ * Email: ludugz@gmail.com
+ */
+
+open class UIState() {
+
+    object Default: UIState()
+    object TimerPage: UIState()
+    object FocusZone: UIState()
+}

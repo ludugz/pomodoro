1bfe90005a737ae0c57d1f0eda3a1d627ce613cc
Tan N. Truong <tntan92@gmail.com>
2024-06-05 22:18:09 +0900

    Init Counter component

---
+++ app/src/main/java/ludugz/pomodoro/ui/components/Counter.kt
@@ -0,0 +1,20 @@
+package ludugz.pomodoro.ui.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+
+/**
+ * Created by Tan N. Truong, on 03 June, 2024
+ * Email: ludugz@gmail.com
+ */
+@Composable
+fun Counter() {
+
+}
+
+@Preview
+@Composable
+fun PreviewCounter() {
+    Counter()
+}

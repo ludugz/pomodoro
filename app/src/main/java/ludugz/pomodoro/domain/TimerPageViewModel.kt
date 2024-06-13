e59e73c61e8d93dd0324da170b8bc20096825e6e
Tan N. Truong <tntan92@gmail.com>
2024-06-13 23:12:26 +0900

    Init some state flow var in TimerPageViewModel.kt

--- app/src/main/java/ludugz/pomodoro/domain/TimerPageViewModel.kt
+++ app/src/main/java/ludugz/pomodoro/domain/TimerPageViewModel.kt
@@ -1,11 +1,19 @@
 package ludugz.pomodoro.domain
 
 import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
 
 
 /**
  * Created by Tan N. Truong, on 13 June, 2024
  * Email: ludugz@gmail.com
  */
+@HiltViewModel
 class TimerPageViewModel : ViewModel() {
+    var counter = MutableStateFlow(0)
+        private set
+
+    var displayDialogCount = MutableStateFlow(0)
+        private set
 }

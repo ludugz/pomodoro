39ee83fe02613611b46b4b95ac347aed87c0d2a2
Tan N. Truong <tntan92@gmail.com>
2024-06-13 21:57:38 +0900

    Init TimerPageViewModel.kt

---
+++ app/src/main/java/ludugz/pomodoro/domain/TimerPageViewModel.kt
@@ -0,0 +1,11 @@
+package ludugz.pomodoro.domain
+
+import androidx.lifecycle.ViewModel
+
+
+/**
+ * Created by Tan N. Truong, on 13 June, 2024
+ * Email: ludugz@gmail.com
+ */
+class TimerPageViewModel : ViewModel() {
+}

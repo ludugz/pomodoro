feb0a6fe7f8a6627a2628f1402a51b2b1e473309
Tan N. Truong <tntan92@gmail.com>
2024-08-21 22:58:25 +0700

    Optimize imports

--- app/src/main/java/ludugz/pomodoro/ui/components/DotGraph.kt
+++ app/src/main/java/ludugz/pomodoro/ui/components/DotGraph.kt
@@ -1,7 +1,23 @@
 package ludugz.pomodoro.ui.components
 
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import ludugz.pomodoro.ui.theme.Lima200
+import ludugz.pomodoro.ui.theme.Lima300
+import java.time.LocalDate
+import java.time.Year
 
 
 /**

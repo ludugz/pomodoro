72c1f77b2a111a4f8a2cde786c31c8b0032dc27b
Tan N. Truong <tntan92@gmail.com>
2024-08-26 11:26:30 +0700

    Update typography

--- app/src/main/java/ludugz/pomodoro/ui/theme/PomoTypography.kt
+++ app/src/main/java/ludugz/pomodoro/ui/theme/PomoTypography.kt
@@ -13,21 +13,21 @@ val PomoTypography = Typography(
         fontFamily = FontFamily.Monospace,
         fontWeight = FontWeight.Normal,
         fontSize = 32.sp,
-        lineHeight = 32.sp,
+        lineHeight = 40.sp,
         letterSpacing = 1.5.sp
     ),
     titleMedium = TextStyle(
         fontFamily = FontFamily.Monospace,
         fontWeight = FontWeight.Normal,
         fontSize = 28.sp,
-        lineHeight = 28.sp,
+        lineHeight = 36.sp,
         letterSpacing = 1.sp
     ),
     titleSmall = TextStyle(
         fontFamily = FontFamily.Monospace,
         fontWeight = FontWeight.Normal,
         fontSize = 24.sp,
-        lineHeight = 24.sp,
+        lineHeight = 32.sp,
         letterSpacing = 0.5.sp
     ),
 
@@ -35,44 +35,45 @@ val PomoTypography = Typography(
     labelLarge = TextStyle(
         fontFamily = FontFamily.Monospace,
         fontWeight = FontWeight.Normal,
-        fontSize = 24.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 1.sp
+        fontSize = 20.sp,
+        lineHeight = 28.sp,
+        letterSpacing = 0.5.sp
     ),
     labelMedium = TextStyle(
         fontFamily = FontFamily.Monospace,
         fontWeight = FontWeight.Normal,
-        fontSize = 20.sp,
-        lineHeight = 20.sp,
-        letterSpacing = 1.sp
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp
     ),
     labelSmall = TextStyle(
         fontFamily = FontFamily.Monospace,
         fontWeight = FontWeight.Normal,
         fontSize = 12.sp,
-        lineHeight = 12.sp,
-        letterSpacing = 1.sp
+        lineHeight = 16.sp,
+        letterSpacing = 0.5.sp
     ),
 
     // Body
     bodyLarge = TextStyle(
         fontFamily = FontFamily.Monospace,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
+        fontSize = 18.sp,
+        lineHeight = 28.sp,
+        letterSpacing = 0.5.sp
     ),
     bodyMedium = TextStyle(
         fontFamily = FontFamily.Monospace,
         fontWeight = FontWeight.Normal,
-        fontSize = 12.sp,
-        lineHeight = 18.sp,
-        letterSpacing = 1.sp
+        fontSize = 14.sp,
+        lineHeight = 22.sp,
+        letterSpacing = 0.25.sp
     ),
     bodySmall = TextStyle(
         fontFamily = FontFamily.Monospace,
         fontWeight = FontWeight.Normal,
-        fontSize = 6.sp,
-        lineHeight = 10.sp,
-        letterSpacing = 1.sp
+        fontSize = 12.sp,
+        lineHeight = 18.sp,
+        letterSpacing = 0.25.sp
     ),
 )

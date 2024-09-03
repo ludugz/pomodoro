bf224e85aa880a045ef25602e4e71158adf57c10
Tan N. Truong <tntan92@gmail.com>
2024-09-03 21:16:26 +0700

    Minor refactoring for Cheering Dialog

--- app/src/main/java/ludugz/pomodoro/ui/components/CheeringDialog.kt
+++ app/src/main/java/ludugz/pomodoro/ui/components/CheeringDialog.kt
@@ -4,13 +4,17 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
@@ -23,6 +27,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -31,13 +36,13 @@ import androidx.compose.ui.window.Dialog
 import ludugz.pomodoro.R
 import ludugz.pomodoro.ui.helpers.Constants.OKAY
 import ludugz.pomodoro.ui.theme.DarkCyan
-import ludugz.pomodoro.ui.theme.PomoTypography
+import ludugz.pomodoro.ui.theme.IbmPlexSansTypography
+import ludugz.pomodoro.ui.theme.MonospaceTypography
 
 /**
  * Created by Tan N. Truong, on 26 December, 2023
  * Email: ludugz@gmail.com
  */
-
 @Composable
 fun CheeringDialog(
     onDismissRequest: () -> Unit,
@@ -60,15 +65,15 @@ fun CheeringDialog(
             Box(modifier = Modifier.fillMaxSize()) {
                 Column(
                     modifier = Modifier
-                        .fillMaxWidth(0.7f)
-                        .fillMaxHeight(0.9f)
-                        .padding(vertical = 16.dp)
+                        .fillMaxWidth(0.8f)
+                        .wrapContentHeight()
+                        .padding(vertical = 8.dp)
                         .background(color = Color.Transparent)
                         .align(Alignment.Center),
                     verticalArrangement = Arrangement.SpaceBetween,
                 ) {
                     Text(
-                        text = "$randomCheeringWord",
+                        text = randomCheeringWord,
                         style = MonospaceTypography.bodyMedium,
                         fontWeight = FontWeight.Thin,
                         fontSize = 12.sp,

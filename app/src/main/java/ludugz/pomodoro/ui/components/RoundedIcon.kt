37391f439f00f6abfd597613f5cabe22c33df713
Tan N. Truong <tntan92@gmail.com>
2024-09-18 23:15:02 +0700

    Update modifier for RoundedIcon

--- app/src/main/java/ludugz/pomodoro/ui/components/RoundedIcon.kt
+++ app/src/main/java/ludugz/pomodoro/ui/components/RoundedIcon.kt
@@ -1,13 +1,17 @@
 package ludugz.pomodoro.ui.components
 
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import ludugz.pomodoro.ui.helpers.Constants.EMPTY_STRING
 import timber.log.Timber
@@ -18,14 +22,19 @@ import timber.log.Timber
  */
 
 @Composable
-fun RoundedIcon(imageVector: ImageVector) {
+fun RoundedIcon(
+    imageVector: ImageVector,
+    sizeInDP: Dp = 36.dp,
     contentDescription: String = EMPTY_STRING,
+) {
     Timber.i("RoundedIcon/${contentDescription} Composable")
     Card(
+        modifier = Modifier.wrapContentSize(),
         onClick = {},
         shape = RoundedCornerShape(4.dp)
     ) {
         Icon(
+            modifier = Modifier.size(size = sizeInDP),
             imageVector = imageVector,
             contentDescription = contentDescription
         )

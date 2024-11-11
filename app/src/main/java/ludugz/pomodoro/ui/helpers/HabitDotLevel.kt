3d85b500f48a394cd926f29defd948cefd522c22
Tan N. Truong <tntan92@gmail.com>
2024-11-11 23:40:17 +0900

    Add enum class for Habit Dot Level

---
+++ app/src/main/java/ludugz/pomodoro/ui/helpers/HabitDotLevel.kt
@@ -0,0 +1,14 @@
+package ludugz.pomodoro.ui.helpers
+
+
+/**
+ * Created by Tan N. Truong, on 11 November, 2024
+ * Email: ludugz@gmail.com
+ */
+enum class HabitDotLevel {
+    NONE,
+    WEAK,
+    NORMAL,
+    STRONG,
+    SUPER
+}

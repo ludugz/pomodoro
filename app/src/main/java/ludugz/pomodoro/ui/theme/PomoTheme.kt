530f195889b3c2440b888c3a29f222e2d65a2004
Tan N. Truong <tntan92@gmail.com>
2024-11-26 23:31:26 +0900

    Update color palette for theme

--- app/src/main/java/ludugz/pomodoro/ui/theme/PomoTheme.kt
+++ app/src/main/java/ludugz/pomodoro/ui/theme/PomoTheme.kt
@@ -10,21 +10,24 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = MidnightBlue,
+    secondary = SkyBlue,
+    tertiary = AquaBlue,
+    onSurface = SoftWhite,
 )
 
 val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = MidnightBlue,
+    secondary = MutedBlue,
+    tertiary = SoftAquaBlue,
+    onSurface = MutedWhite,
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),

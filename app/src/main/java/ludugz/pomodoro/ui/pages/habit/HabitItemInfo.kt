9edaac86d3ae301571e1b4890fac622739d64eac
Tan N. Truong <tntan92@gmail.com>
2024-12-10 21:53:14 +0900

    Init Habit Item Info data class

---
+++ app/src/main/java/ludugz/pomodoro/ui/pages/habit/HabitItemInfo.kt
@@ -0,0 +1,13 @@
+package ludugz.pomodoro.ui.pages.habit
+
+import ludugz.pomodoro.ui.components.Dot
+
+
+/**
+ * Created by Tan N. Truong, on 10 December, 2024
+ * Email: ludugz@gmail.com
+ */
+data class HabitItemInfo(
+    val dots: List<Dot>,
+    var name: String,
+)

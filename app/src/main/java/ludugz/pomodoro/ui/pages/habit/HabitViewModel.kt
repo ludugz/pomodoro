eebf262574d96f3c1c3b7b1a95cad2dd3ded61ac
Tan N. Truong <tntan92@gmail.com>
2024-12-16 13:44:51 +0900

    Init Habit View Model

---
+++ app/src/main/java/ludugz/pomodoro/ui/pages/habit/HabitViewModel.kt
@@ -0,0 +1,11 @@
+package ludugz.pomodoro.ui.pages.habit
+
+import androidx.lifecycle.ViewModel
+
+
+/**
+ * Created by Tan N. Truong, on 16 December, 2024
+ * Email: ludugz@gmail.com
+ */
+class HabitViewModel : ViewModel() {
+}
